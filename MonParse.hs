2cdcbffb00d01a4eb0d05e016c229c13afd2d8fc
injuly <srijannr1@gmail.com>
2022-03-23 23:25:32 +0530

    make Parser an instance of Monad

--- MonParse.hs
+++ MonParse.hs
@@ -1,43 +1,46 @@
 -- https://www.cs.nott.ac.uk/~pszgmh/monparsing.pdf
 -- Haskell implementation of the monadic parser combination strategy
--- presented in the paper above. For simplicity, and to avoid subclassing
--- `MonadZero`, `Monad`, `Applicative` and `Functor`,
--- we use a custom operator `>>>` as if it were the monadic bind, and corresponding
--- equivalent functions to act as `return`, `zero` etc.
+-- presented in the paper above.
 
+import Control.Monad (MonadPlus (..))
 import Data.Char (isDigit, isLower, isUpper, ord)
 
 -- Empty list -> Parse error
 -- [(x, xs)] -> x is the parse tree and `xs` is the rest of the unparsed string.
-type Parser a = String -> [(a, String)]
+type ParseFun a = String -> [(a, String)]
+
+newtype Parser a = Parser {parse :: ParseFun a}
 
 -- Produces the parse tree `v` and leaves the input string untouched.
 result :: a -> Parser a
-result v inp = [(v, inp)]
+result v = Parser $ \inp -> [(v, inp)]
 
--- A parser that always fails
-zero :: Parser a
-zero _ = []
+instance Monad Parser where
+  p >>= f = Parser $ \inp ->
+    concat [parse (f v) inp' | (v, inp') <- parse p inp]
+  return = result
+
+instance MonadPlus Parser where
+  -- A parser that always fails by returning an empty list
+  mzero = Parser $ const []
+  p `mplus` q = Parser $ \inp -> parse p inp ++ parse q inp
 
 -- Consumes a single character from the input string
 item :: Parser Char
-item [] = []
-item (x : xs) = [(x, xs)]
-
--- The `bind` operation.
--- If our Parser type was an actual Monad instance, this would be `>>=`
-( >>> ) :: Parser a -> (a -> Parser b) -> Parser b
-p >>> f = \inp -> concat [f v inp' | (v, inp') <- p inp]
+item = Parser parseItem
+  where
+    parseItem [] = []
+    parseItem (x : xs) = [(x, xs)]
 
 -- Takes a predicate (a Boolean valued function),
 -- and yields a parser that consumes a single character if it
 -- satisfies the predicate, and fails otherwise
 sat :: (Char -> Bool) -> Parser Char
 sat p =
-  item >>> \x ->
+  item >>= \x ->
     if p x
       then result x
-      else zero
+      else mzero
 
 -- Matches a specific character
 char :: Char -> Parser Char
@@ -58,7 +61,7 @@ upper = sat isUpper
 -- Applies two parsers to the same input, then returns a list
 -- containing results returned by both of them.
 plus :: Parser a -> Parser a -> Parser a
-p `plus` q = \inp -> p inp ++ q inp
+p `plus` q = Parser $ \inp -> parse p inp ++ parse q inp
 
 -- Consumes any letter.
 letter :: Parser Char
@@ -72,16 +75,16 @@ alphanum = letter `plus` digit
 string :: String -> Parser String
 string "" = result ""
 string (x : xs) =
-  char x >>> \_ ->
-    string xs >>> \_ ->
-      result (x : xs)
+  char x
+    >> string xs
+    >> result (x : xs)
 
 -- Applies two parsers `p` and `q` to the input string.
 -- If `p` parses the string into something successful, its parse result is returned.
 -- If `p` fails but `q` parses successfully, the parse result of `q` is returned.
 -- If both fail, an empty list is returned.
 (+++) :: Parser a -> Parser a -> Parser a
-(p +++ q) inp = case (p `plus` q) inp of
+p +++ q = Parser $ \inp -> case parse (p `mplus` q) inp of
   [] -> []
   (x : xs) -> [x]
 
@@ -89,43 +92,43 @@ string (x : xs) =
 -- e.g - `many digit "123ABC"` returns [("123", "ABC")]
 many :: Parser a -> Parser [a]
 many p =
-  ( p >>> \x -> -- Start by applying p once
-      many p >>> \xs -> -- Then recursive apply `p` many times.
+  ( p >>= \x -> -- Start by applying p once
+      many p >>= \xs -> -- Then recursive apply `p` many times.
         result (x : xs) -- Finally, combine the results
   )
     +++ result [] -- In case `p` fails to apply either in the initial call, or in one of the
     -- recursive calls to itself, we return an empty list instead.
 
--- Applies 
+-- Applies
 word :: Parser String
 word = many letter
 
 -- Consumes a pattern that matches `[a-zA-Z][a-zA-Z0-9]*`
 ident :: Parser String
 ident =
-  letter >>> \x -> -- one letter, followed by...
-    many alphanum >>> \xs -> -- zero or more alphanumeric chars
+  letter >>= \x -> -- one letter, followed by...
+    many alphanum >>= \xs -> -- zero or more alphanumeric chars
       result (x : xs)
 
 -- Not in the paper, just something I was playing around with.
 -- Applies `p` first, then `q`, and returns the results in a 2-tuple.
 then' :: Parser a -> Parser b -> Parser (a, b)
 then' p q =
-  p >>> \x ->
-    q >>> \xs ->
+  p >>= \x ->
+    q >>= \xs ->
       result (x, xs)
 
 -- Same as many, but only works if `p` can be applied at least once. (`+` in regex)
 many1 :: Parser a -> Parser [a]
 many1 p =
-  p >>> \x ->
-    many p >>> \xs ->
+  p >>= \x ->
+    many p >>= \xs ->
       result (x : xs)
 
 -- Consumes a natural number
 nat :: Parser Int
 nat =
-  many1 digit >>> eval
+  many1 digit >>= eval
   where
     eval xs = result $ foldl1 op [ord x - ord '0' | x <- xs]
     m `op` n = 10 * m + n
@@ -135,6 +138,6 @@ nat =
 -- contains the parse results of `p`, separated by the parse results of `sep`.
 sepby1 :: Parser a -> Parser b -> Parser [a]
 p `sepby1` sep =
-  p >>> \x ->  -- first, apply `p`
-    many (sep >>> const p) -- To the rest of the string, apply `many (sep >>> const p)`
-      >>> \xs -> result (x : xs) --  Then combine the two results into a list and return it.
+  p >>= \x -> -- first, apply `p`
+    many (sep >>= const p) -- To the rest of the string, apply `many (sep >>= const p)`
+      >>= \xs -> result (x : xs) --  Then combine the two results into a list and return it.
